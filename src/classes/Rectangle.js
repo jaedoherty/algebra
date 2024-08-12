2dfa4ac1b302f2f3a7b1c15d6dc41a4e78e673f2
Jae Doherty <jaedoherty@Jaes-MacBook-Pro.local>
2024-08-12 12:54:03 -0700

    Make rectangles selectable

---
+++ src/classes/Rectangle.js
@@ -0,0 +1,63 @@
+class Rectangle {
+  constructor(
+    context,
+    x,
+    y,
+    width,
+    height,
+    border,
+    background,
+    text,
+    textAlign
+  ) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.border = border;
+    this.background = background;
+    this.text = text;
+    this.textAlign = textAlign;
+    this.isSelected = false;
+  }
+
+  onClick(isSelected) {
+    this.isSelected = isSelected
+  }
+
+  containsCoordinate(x, y) {
+    const maxX = this.x + this.width
+    const maxY = this.y + this.height
+
+    return x >= this.x && x <= maxX && y >= this.y && y <= maxY
+  }
+
+  draw() {
+    if (this.isSelected) {
+        this.context.beginPath()
+        this.context.roundRect(this.x - 10, this.y - 10, this.width + 20, this.height + 20, 5)
+        this.context.strokeStyle = "blue";
+        this.context.stroke();
+    } 
+    // Hard code font
+    this.context.font = "16px Kalam";
+
+    this.context.beginPath();
+    this.context.roundRect(this.x, this.y, this.width, this.height, 5);
+    this.context.fillStyle = this.background;
+    this.context.fill();
+    this.context.strokeStyle = this.border;
+    this.context.stroke();
+    this.context.fillStyle = "#000000";
+    this.context.textAlign = this.textAlign;
+    this.context.textBaseline = "middle";
+    this.context.fillText(this.text, this.x + this.width / 2, this.y + this.height / 2 + 2);
+
+    // Reset fill and stroke styles
+    this.context.fillStyle = "rgba(0,0,0,0)";
+    this.context.strokeStyle = "#000000";
+  }
+}
+
+export default Rectangle;
--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,40 +1,94 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from "react";
 
-import drawRect from '../../utils/canvas/drawRect'
-import { blockBorderColors, blockBackgroundColors } from '../FractionBlock/constants'
+import {
+  blockBorderColors,
+  blockBackgroundColors,
+} from "../FractionBlock/constants";
+import Rectangle from "../../classes/Rectangle";
 
-import "./Canvas.css"
+import "./Canvas.css";
 
 const Canvas = () => {
-    const canvasRef = useRef(null)
+  const canvasRef = useRef(null);
+  const [shapes, setShapes] = useState([]);
 
-    const onDragOver = (e) => {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = "move";
+  const onDragOver = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  const onDrop = (e) => {
+    const data = e.dataTransfer.getData("text/plain")?.split(",");
+    const [id, xOffset, yOffset] = data;
+    const dropX = e.clientX - xOffset;
+    const dropY = e.clientY - yOffset;
+    const denom = id[id.length - 1];
+    const width = 200 / denom;
+    const text = denom === "1" ? "1" : `1/${denom}`;
+    const rectangle = new Rectangle(
+      canvasRef.current.getContext("2d"),
+      dropX,
+      dropY,
+      width,
+      35,
+      blockBorderColors[id],
+      blockBackgroundColors[id],
+      text,
+      "center"
+    );
+    const newShapes = [...shapes, rectangle];
+    setShapes(newShapes);
+  };
+
+  useEffect(() => {
+    // Set up canvas
+    if (canvasRef.current) {
+      canvasRef.current.height = canvasRef.current.clientHeight;
+      canvasRef.current.width = canvasRef.current.clientWidth;
     }
+  }, []);
 
-    const onDrop = (e) => {
-        const data = e.dataTransfer.getData("text/plain")?.split(',');
-        const [id, xOffset, yOffset] = data;
-        const dropX = e.clientX - xOffset;
-        const dropY = e.clientY - yOffset;
-        const denom = id[id.length - 1]
-        const width = 200 / denom
-        const text = denom === '1' ? '1' : `1/${denom}`
-        drawRect(canvasRef.current, dropX, dropY, width, 35, blockBorderColors[id], blockBackgroundColors[id], text)
-    }
+  useEffect(() => {
+    canvasRef.current
+      .getContext("2d")
+      .clearRect(
+        0,
+        0,
+        canvasRef.current.clientWidth,
+        canvasRef.current.clientHeight
+      );
+    shapes.forEach((shape) => {
+      shape.draw();
+    });
+  }, [shapes]);
 
-    useEffect(() => {
-        // Set up canvas
-        if (canvasRef.current) {
-            canvasRef.current.height = canvasRef.current.clientHeight;
-            canvasRef.current.width = canvasRef.current.clientWidth;
+  const onClick = (e) => {
+    let hasSelected = false
+    for (const shape of shapes.toReversed()) {
+      if (shape.containsCoordinate(e.clientX, e.clientY)) {
+        if (!hasSelected) {
+            shape.onClick(true);
+            hasSelected = true
+        } else {
+            shape.onClick(false);
         }
-    }, [])
+      } else {
+        shape.onClick(false);
+      }
+      const newShapes = [...shapes];
+      setShapes(newShapes);
+    }
+  };
 
-    return (
-        <canvas ref={canvasRef} id="canvas" onDragOver={onDragOver} onDrop={onDrop}></canvas>
-    )
-}
+  return (
+    <canvas
+      ref={canvasRef}
+      id="canvas"
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+      onClick={onClick}
+    ></canvas>
+  );
+};
 
 export default Canvas;
