74bd735a02028ac7b26b221f482b823daea7beb0
Jae Doherty <jaedoherty@Jaes-MacBook-Pro.local>
2024-08-14 13:17:29 -0700

    Add active state to remaining Toolbar buttons. Change Test Button to Cards

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -6,25 +6,72 @@ import "./Toolbar.css";
 
 const Toolbar = (props) => {
   const [isFractionBlocksOpen, setIsFractionBlocksOpen] = useState(false);
+  const [isToggleGridOpen, setIsToggleGridOpen] = useState(false);
+  const [isAlgebraBlocksOpen, setIsAlgebraBlocksOpen] = useState(false);
+  const [isCardsOpen, setIsCardsOpen] = useState(false);
+
+  const handleFractionBlocksClick = () => {
+    setIsFractionBlocksOpen(!isFractionBlocksOpen);
+    setIsToggleGridOpen(false);
+    setIsAlgebraBlocksOpen(false);
+    setIsCardsOpen(false);
+  };
+
+  const handleToggleGridClick = () => {
+    setIsToggleGridOpen(!isToggleGridOpen);
+    setIsFractionBlocksOpen(false);
+    setIsAlgebraBlocksOpen(false);
+    setIsCardsOpen(false);
+  };
+
+  const handleAlgebraBlocksClick = () => {
+    setIsAlgebraBlocksOpen(!isAlgebraBlocksOpen);
+    setIsFractionBlocksOpen(false);
+    setIsToggleGridOpen(false);
+    setIsCardsOpen(false);
+  };
+
+  const handleCardsClick = () => {
+    setIsCardsOpen(!isCardsOpen);
+    setIsFractionBlocksOpen(false);
+    setIsToggleGridOpen(false);
+    setIsAlgebraBlocksOpen(false);
+  };
+
   return (
     <menu className="toolbar">
       <li className="toolbar-item">
         <ToolbarButton
           name="Fraction Blocks"
           color="blue"
-          onClick={() => setIsFractionBlocksOpen(!isFractionBlocksOpen)}
+          onClick={handleFractionBlocksClick}
           isActive={isFractionBlocksOpen}
         />
         {isFractionBlocksOpen && <FractionBlocks />}
       </li>
       <li className="toolbar-item">
-        <ToolbarButton name="Toggle Grid" color="green" />
+        <ToolbarButton
+          name="Toggle Grid"
+          color="green"
+          onClick={handleToggleGridClick}
+          isActive={isToggleGridOpen}
+        />
       </li>
       <li className="toolbar-item">
-        <ToolbarButton name="Algebra Blocks" color="red" />
+        <ToolbarButton
+          name="Algebra Blocks"
+          color="red"
+          onClick={handleAlgebraBlocksClick}
+          isActive={isAlgebraBlocksOpen}
+        />
       </li>
       <li className="toolbar-item">
-        <ToolbarButton name="Test Button" color="yellow" />
+        <ToolbarButton
+          name="Cards"
+          color="yellow"
+          onClick={handleCardsClick}
+          isActive={isCardsOpen}
+        />
       </li>
     </menu>
   );
